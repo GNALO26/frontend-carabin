90f9fb29de410d18d88d701e8addfae08ce82531
Alfred SOSSA <alfredsossa17@gmail.com>
2025-08-20 11:38:23 +0100

    Refactor App.jsx with AdminLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+
+// Pages publiques & utilisateurs
 import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
 import DashboardPage from './pages/DashboardPage';
 import PaymentPage from './pages/PaymentPage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
+
+// Pages Admin
 import AdminLoginPage from './admin/LoginPage';
 import AdminDashboard from './admin/DashboardPage';
 import QuizEditor from './admin/QuizEditor';
 import QuizzesPage from './admin/QuizzesPage';
+
+// Layouts
 import Layout from './components/layout/Layout';
+import AdminLayout from './components/layout/AdminLayout';
+
+// Routes sécurisées
 import PrivateRoute from './components/auth/PrivateRoute';
 import AdminRoute from './components/auth/AdminRoute';
 
@@ -19,32 +28,36 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <Layout>
-          <Routes>
+        <Routes>
+          {/* --------- Layout Public --------- */}
+          <Route element={<Layout />}>
             {/* Routes publiques */}
             <Route path="/" element={<HomePage />} />
             <Route path="/quiz/:id" element={<QuizPage />} />
             <Route path="/login" element={<LoginPage />} />
-            
+
             {/* Routes utilisateur authentifié */}
             <Route element={<PrivateRoute />}>
               <Route path="/dashboard" element={<DashboardPage />} />
               <Route path="/payment" element={<PaymentPage />} />
             </Route>
-            
-            {/* Routes admin */}
-            <Route path="/admin/login" element={<AdminLoginPage />} />
-            <Route element={<AdminRoute />}>
+          </Route>
+
+          {/* --------- Layout Admin --------- */}
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+
+          <Route element={<AdminRoute />}>
+            <Route element={<AdminLayout />}>
               <Route path="/admin/dashboard" element={<AdminDashboard />} />
               <Route path="/admin/quizzes" element={<QuizzesPage />} />
               <Route path="/admin/quizzes/new" element={<QuizEditor />} />
               <Route path="/admin/quizzes/edit/:id" element={<QuizEditor />} />
             </Route>
-            
-            {/* Page 404 */}
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Layout>
+          </Route>
+
+          {/* --------- 404 --------- */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </Router>
     </AuthProvider>
   );
