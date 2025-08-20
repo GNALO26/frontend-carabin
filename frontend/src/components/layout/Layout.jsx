2c980d32b0b441252ecf205bffd3bd2163c1f443
Alfred SOSSA <alfredsossa17@gmail.com>
2025-08-20 15:38:37 +0100

    Mise à jour du projet : corrections Layout, RegisterPage et AdminLayout

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow container mx-auto p-4">
-        {children}
+        <Outlet />  {/* 🔥 Ici React Router injectera Home, Register, Login, etc. */}
       </main>
       <Footer />
     </div>
