99015279e6561b0e443e922d7ba4b611d07b0a07
Alfred SOSSA <alfredsossa17@gmail.com>
2025-08-22 00:15:52 +0100

    Fix Netlify build: set base to frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      {/* Navigation moderne inspirée d'Asin.bj */}
+      {/* Navigation moderne */}
       <nav className="bg-white shadow-md sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
@@ -340,8 +340,8 @@ const HomePage = () => {
                       Défis exclusifs
                     </li>
                     <li className="flex items-center">
-                    <svg className="h-5 w-5 text-green-500 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                      <svg className="h-5 w-5 text-green-500 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                       </svg>
                       Support prioritaire
                     </li>
@@ -383,10 +383,10 @@ const HomePage = () => {
         </div>
       </section>
 
-      {/* Footer moderne */}
+      {/* Footer moderne sans icônes de réseaux sociaux */}
       <footer className="bg-gray-900 text-white py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div>
               <span className="text-2xl font-bold flex items-center">
                 <svg className="w-6 h-6 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -408,36 +408,10 @@ const HomePage = () => {
             </div>
             
             <div>
-              <h4 className="text-lg font-semibold mb-4">Légal</h4>
-              <ul className="space-y-2">
-                <li><Link to="/privacy" className="text-gray-400 hover:text-white transition-colors">Politique de confidentialité</Link></li>
-                <li><Link to="/terms" className="text-gray-400 hover:text-white transition-colors">Conditions d'utilisation</Link></li>
-                <li><Link to="/cookies" className="text-gray-400 hover:text-white transition-colors">Politique des cookies</Link></li>
-              </ul>
-            </div>
-            
-            <div>
               <h4 className="text-lg font-semibold mb-4">Contact</h4>
               <ul className="space-y-2">
                 <li className="text-gray-400">quizdecarabin4@gmail.com</li>
                 <li className="text-gray-400">+229 XX XX XX XX</li>
-                <li className="flex space-x-4 pt-2">
-                  <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12z" />
-                    </svg>
-                  </a>
-                  <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                  </a>
-                  <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M12 2C6.477 2 2 6.477 2 12c0 5.523 4.477 10 10 10s10-4.477 10-10c0-5.523-4.477-10-10-10zm3.7 14.077c-1.75.362-5.453.362-7.203 0-1.896-.391-2.117-1.538-2.246-2.077-.02-.086-.029-.176-.029-.265v-2.97c0-.182.108-.35.27-.431l2.5-1.423c.094-.055.204-.055.298 0l2.5 1.423c.161.08.27.25.27.431v2.97c0 .089-.01.179-.03.265-.056.242-.17.642-1.017.898.26.432.908 1.01 1.746 1.45 1.703.888 4.026 1.088 5.633.313.32-.154.48-.519.352-.85-.25-.656-.985-.954-1.643-.711zm-7.1-4.873v1.719c-.002.115.033.226.094.317.224.392.893.517 1.322.234.12-.08.206-.2.243-.337.026.082.064.16.112.232.224.392.893.517 1.322.234.12-.08.206-.2.243-.337.026.082.064.16.112.232.225.392.894.517 1.322.234.12-.08.206-.2.244-.337.044.137.12.262.222.363.3.3.788.3 1.088 0 .3-.3.3-.788 0-1.088l-.008-.008v-2.318l-2.0-1.14-2.0 1.14v1.718zm7.5 2.0v.5c0 .093-.02.185-.06.27-.224.392-.893.517-1.322.234-.12-.08-.206-.2-.243-.337-.026.082-.064.16-.112.232-.224.392-.893.517-1.322.234-.12-.08-.206-.2-.243-.337-.026.082-.064.16-.112.232-.225.392-.894.517-1.322.234-.12-.08-.206-.2-.244-.337-.044.137-.12.262-.222.363-.3.3-.788.3-1.088 0-.3-.3-.3-.788 0-1.088v-2.5c0-.093.02-.185.06-.27.225-.392.894-.517 1.322-.234.12.08.206.2.243.337.026-.082.064-.16.112-.232.225-.392.894-.517 1.322-.234.12.08.206.2.243.337.026-.082.064-.16.112-.232.224-.392.893-.517 1.322-.234.12.08.206.2.243.337.026-.082.064-.16.112-.232.224-.392.893-.517 1.322-.234.12.08.206.2.243.337.044-.137.12-.262.222-.363.3-.3.788-.3 1.088 0 .3.3.3.788 0 1.088v2.5z" />
-                    </svg>
-                  </a>
-                </li>
               </ul>
             </div>
           </div>
