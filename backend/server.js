384f2db05ce4bca79922f7a0176dffb8834bcff4
Alfred SOSSA <alfredsossa17@gmail.com>
2025-08-27 13:23:35 +0100

    force add router.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const router = require("./router"); // centralisation routes
+const router = require("./router.js"); // centralisation routes
 
 const app = express();
 
