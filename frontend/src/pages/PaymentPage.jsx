219ce65bc3e2b750da916ee2eab51e240a1db5d2
Alfred SOSSA <alfredsossa17@gmail.com>
2025-09-02 13:25:53 +0100

    deployment

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { formatPhoneNumber, validatePhoneNumber } from "../services/payment";
+import paymentService, { formatPhoneNumber, validatePhoneNumber } from "../services/payment";
 import { useIsMounted } from "../hooks";
 
 const PaymentPage = () => {
