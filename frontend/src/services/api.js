2d13a5ccd5de1cb8de4cb092ff78186ac4e0c474
Alfred SOSSA <alfredsossa17@gmail.com>
2025-09-05 01:53:53 +0100

    deploiement

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://quiz-de-carabin-backend.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://quiz-de-carabin-backend.onrender.com/api';
 
 const API = axios.create({
   baseURL: API_BASE_URL,
